a56ff378c6f0d79a8859d4246a071f17a8a874be
Hayley Dobbs <iamdobbs@icloud.com>
2023-02-27 15:46:52 +0000

    Added code to index.js to return array in console log

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
+const NotesModel = require('./notesModel')
+
 console.log('The notes app is running');
+
+
+let notemodel = new NotesModel;
+notemodel.addNote('Buy Milk');
+notemodel.addNote('Go to the Gym');
+console.log(notemodel.getNotes());
