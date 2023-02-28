2f6d213a565ae32693933a70782b3cadbd9cae8a
Hayley Dobbs <iamdobbs@icloud.com>
2023-02-28 14:11:46 +0000

    Created a new model, added note and called displayNotes in index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const NotesModel = require('./notesModel')
+const NotesView = require('./notesView')
 
 console.log('The notes app is running');
 
@@ -7,3 +8,8 @@ let notemodel = new NotesModel;
 notemodel.addNote('Buy Milk');
 notemodel.addNote('Go to the Gym');
 console.log(notemodel.getNotes());
+
+const model = new NotesModel();
+model.addNote('This is an example note')
+const view = new NotesView(model);
+view.displayNotes();
